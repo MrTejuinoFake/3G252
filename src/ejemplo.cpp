16f7292379cdad2de6164442f4fbb457fbbec96f
MrTejuinoFake <a24310381@live.ceti.mx>
2025-10-18 01:34:48 +0000

    PRACTICA 4

--- src/ejemplo.cpp
+++ src/ejemplo.cpp
@@ -1,33 +1,66 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
-//#include<ftxui/component/component.hpp>
-//#include<ftxui/component/screen_interactive.hpp>
-#include<ftxui/screen/screen.hpp>
-#include<ftxui/screen/color.hpp>
+// #include<ftxui/component/component.hpp>
+// #include<ftxui/component/screen_interactive.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
 
 using namespace ftxui;
 
+string nave =
+    string("     ___     \n") +
+    string(" ___/   \\___ \n") +
+    string("/   '---'   \\  \n") +
+    string("'--_______--'\n") +
+    string("     / \\      \n") +
+    string("    /   \\      \n") +
+    string("    /\\O/\\      \n") +
+    string("    / | \\      \n") +
+    string("    // \\    \n");
 
-int main(int argc,char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     auto pantalla = Screen::Create(
-        Dimension::Full(   ),
-        Dimension::Fixed(10)    
-    );
+        Dimension::Full(),
+        Dimension::Fixed(10));
 
-    int x=0;
-    int y=0;
+    int x = 0;
+    int y = 0;
+   
 
-   auto& pixel = pantalla.PixelAt(5,5); 
-   pixel.blink = true;
-    pixel.background_color = Color::Blue;
-    pixel.foreground_color = Color::Red;
-    pixel.character = 'A';
+    while (true)
+    {
+        x++; 
+        stringstream ss(nave);
+        string linea;
+        int nolinea = 0;
+        while (getline(ss, linea))
+        {
+            int columna = 0;
+            for (auto &&letra : linea)
+            {
+                auto &pixel = pantalla.PixelAt(x + columna, y + nolinea);
+                // pixel.blink = true;
+                // pixel.background_color = Color::Blue;
+                // pixel.foreground_color = Color::Red;
+                pixel.character = letra;
+                columna++;
+            }
+            nolinea++;
+        }
 
-pantalla.Print();
-//cout<<pantalla.ToString()<<endl;
+        pantalla.Print();
+        pantalla.Clear();
+        cout << pantalla.ResetPosition();
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+    // cout<<pantalla.ToString()<<endl;
 
-
-    //cout << "compila" << endl;
+    // cout << "compila" << endl;
     return 0;
 }
